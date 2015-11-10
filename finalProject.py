3cae719379ea891bc5070c449a9a4059c8a48313
thiagos <thiagossobral@gmail.com>
2015-11-09 22:11:13 -0500

    Add templates for restaurant level operations

--- finalProject.py
+++ finalProject.py
@@ -1,35 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Making an API Endpoint (GET Request)
-@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
-def showRestaurantJson(restaurant_id):
-    restaurant = restaurant_crud.getRestaurant(restaurant_id)
-    rest_items = restaurant_crud.getRestaurantItems(restaurant.id)
-    return jsonify(MenuItems=[i.serialize for i in rest_items])
+# Temp code, for templates basic test
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
 
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_item_id>/JSON')
-def showRestaurantItemJson(restaurant_id, menu_item_id):
-    rest_item = restaurant_crud.getMenuItem(menu_item_id)
-    return jsonify(rest_item.serialize)
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+
+def getRest(id):
+    for rest in restaurants:
+        if int(rest['id']) == id:
+            return rest
+
+# END Temp code, for templates basic test
 
 @app.route('/')
 @app.route('/restaurants/')
 def showRestaurants():
-    return "page to show all restaurants!"
+    return render_template('restaurants.html', restaurants=restaurants)
 
-@app.route('/restaurants/new')
+@app.route('/restaurants/new', methods=['GET', 'POST'])
 def createRestaurant():
-    return "page to create a restaurant!"
+    return render_template('newrestaurant.html')
 
 @app.route('/restaurants/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-    return "page to edit a restaurant!"
+    restaurant = getRest(restaurant_id)
+    print restaurant
+    return render_template('editrestaurant.html', restaurant=restaurant)
 
 @app.route('/restaurants/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-    return "page to delete a restaurant!"
+    restaurant = getRest(restaurant_id)
+    return render_template('deleterestaurant.html', restaurant=restaurant)
 
 @app.route('/restaurants/<int:restaurant_id>/menu')
 def showRestaurant(restaurant_id):
