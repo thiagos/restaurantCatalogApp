093378d5e5dc23942195f3cf7be2a0a735d0be1c
thiagos <thiagossobral@gmail.com>
2015-11-14 19:30:23 -0500

    Add database setup with Restaurant and MenuItem classes

---
+++ database_setup.py
@@ -0,0 +1,42 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Restaurant(Base):
+
+    __tablename__ = 'restaurant'
+
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True, autoincrement = True)
+    menu_items = relationship("MenuItem", backref='restaurant', cascade='all, delete-orphan')
+
+
+class MenuItem(Base):
+
+    __tablename__ = 'menu_item'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250))
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+
+    @property
+    def serialize(self):
+        # Returns object data in easily serializeable format
+        return {
+            'name' : self.name,
+            'description' : self.description,
+            'id' : self.id,
+            'price' : self.price,
+            'course' : self.course,
+        }
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
